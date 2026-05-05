55414352d81662a809803ab728dde5545cf94cca
Rafael <rafaelmachadocd37@gmail.com>
2026-05-05 15:17:04 -0300

    feat: Interactive sum, subtraction and division of matrices

--- src/main.c
+++ src/main.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include "addMatrix.c"
+#include "subMatrix.c"
+#include "MultiMatrix.c"
+#include "DivMatrix.c"
+#include "printMatrix.c"
+#include "scanMatrix.c"
 
 int main()
 {
@@ -18,7 +25,9 @@ int main()
  *            \/     \/          \/            
  */
 
-	while(1)
+    bool running = true;
+
+	while(running)
 	{
 			int ordem;
 			char operacao;
@@ -34,28 +43,84 @@ int main()
 			"> "
 		);
 
-		scanf("%s",&operacao);
+        int matrixA[ordem][ordem];
+        int matrixB[ordem][ordem];
+
+		scanf(" %c", &operacao);
 
 		switch(operacao)
 		{
 		    case '+':
-					
+            {
+                int sum[ordem][ordem];
+
+                scanMatrix(ordem, matrixA);
+                printf("\n");
+                scanMatrix(ordem, matrixB);
+
+                addMatrix(ordem, matrixA, matrixB, sum);
+
+                printf("\nA matriz resultante é:\n\n");
+                printMatrix(ordem, sum);
+                printf("\n\n");
+
+                running = false;
 				break;
+            }
+
 			case '-':
-					
+            {
+                int sub[ordem][ordem];
+
+                scanMatrix(ordem, matrixA);
+                printf("\n");
+                scanMatrix(ordem, matrixB);
+
+                subMatrix(ordem, matrixA, matrixB, sub);
+
+                printf("\nA matriz resultante é:\n\n");
+                printMatrix(ordem, sub);
+                printf("\n\n");
+
+                running = false;
 				break;
+            }
+
 			case '*':
-					
+            {
+
+
+                running = false;
 				break;
+            }
+
 			case '/':
-					
+            {
+    
+                int div[ordem][ordem];
+
+                scanMatrix(ordem, matrixA);
+                printf("\n");
+                scanMatrix(ordem, matrixB);
+
+                // Se tiver divisão por 0, quebra tudo
+                divMatrix(ordem, matrixA, matrixB, div);
+
+                printf("\nA matriz resultante é:\n\n");
+                printMatrix(ordem, div);
+                printf("\n\n");
+
+                running = false;
 				break;
+            }
+
 			default:
 				printf("Digite uma operação válida!\n");
+                running = false;
 				break;
 		}
 
-
+        break;
 
 
 
