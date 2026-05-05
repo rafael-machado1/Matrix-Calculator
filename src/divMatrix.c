4db668fe313245d7dbe2019bb2e9c70fcbead300
Rafael <rafaelmachadocd37@gmail.com>
2026-05-05 15:15:21 -0300

    feat: Matrix division

---
+++ src/divMatrix.c
@@ -0,0 +1,22 @@
+#include "../include/div.h"
+
+void divMatrix(int tamanho, int a[tamanho][tamanho], int b[tamanho][tamanho], int div[tamanho][tamanho])
+{
+	for (int i = 0; i < tamanho; i++)
+	{
+		for (int j = 0; j < tamanho; j++)
+		{
+            // Divisão por zero
+            if (b[i][j] == 0)
+            {
+                printf("\n\nDivisão por zero\n");
+                break;
+                break;
+            }
+            else
+            {
+                div[i][j] = a[i][j] / b[i][j];
+            }
+		}
+	}
+}
