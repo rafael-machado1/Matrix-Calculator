ffb9247c9536904eb3e42eb2e5272d5ef9a3464c
Rafael <rafaelmachadocd37@gmail.com>
2026-05-05 11:07:30 -0300

    feat: Header sub function

--- include/sub.h
+++ include/sub.h
@@ -1,3 +1,4 @@
 #pragma once
 
-void sub(float i, float j);
+// Chamando na main seria (exemplo de 3x3): sub(3, a, b, sub)
+void sub(int tamanho, int a[tamanho][tamanho], int b[tamanho][tamanho], int sub[tamanho][tamanho]);
