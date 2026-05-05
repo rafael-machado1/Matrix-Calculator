2648c19b3580b79a274ee818098338737708aecc
Rafael <rafaelmachadocd37@gmail.com>
2026-05-05 10:40:56 -0300

    feat: Header add function

--- include/add.h
+++ include/add.h
@@ -3,4 +3,4 @@
 // Header para soma de duas matrizes
 // Chamando na main seria (exemplo de 3x3): add(3, a, b, sum)
 // É necessário declarar sum para que ele retorne após o fim da função
-void add(int tamanho, int a[tamanho][tamanho], int b[tamanho][tamanho], int sum[tamanho][tamanho])
+void add(int tamanho, int a[tamanho][tamanho], int b[tamanho][tamanho], int sum[tamanho][tamanho]);
