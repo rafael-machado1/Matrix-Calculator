5a9bcaa3b0d4a56fa6a745451572ee7e260f8eda
Rafael <rafaelmachadocd37@gmail.com>
2026-05-07 13:13:00 -0300

    feat: Determinant

---
+++ include/determinant.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void determinant(int tamanho, a[tamanho][tamanho]);
---
+++ src/determinant.c
@@ -0,0 +1,6 @@
+#include "../include/determinant.h"
+
+int determinant(int tamanho, a[tamanho][tamanho], resultado)
+{
+
+}
