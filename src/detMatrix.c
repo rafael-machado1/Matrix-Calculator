5055edd29276b5871056bc65380f88936d15698e
Rafael <rafaelmachadocd37@gmail.com>
2026-05-08 14:04:48 -0300

    refactor: Now using recursivity for determinant

--- src/detMatrix.c
+++ src/detMatrix.c
@@ -1,26 +1,26 @@
 #include "../include/determinant.h"
 
-void determinant(int tamanho, a[tamanho][tamanho], det)
+int detMatrix(int tamanho, int a[tamanho][tamanho])
 {
     if (tamanho == 1)
     {
-        resultado = a[0][0];
+        return a[0][0];
     }
     else if (tamanho == 2)
     {
-        resultado = a[0][0] * a[1][1] + a[1][0] * a[0][1];
+        return a[0][0] * a[1][1] - a[1][0] * a[0][1];
     }
-    else
-    {
-        for (int i = 0; i < tamanho; i++)
-        {
-            for (int j = 0; j < tamanho; j++)
-            {
-                for (int k = 0; k < tamanho; k++)
-                {
-                                                           
-                }
-            }
-        }
-    }
+    // else
+    // {
+    //     for (int i = 0; i < tamanho; i++)
+    //     {
+    //         for (int j = 0; j < tamanho; j++)
+    //         {
+    //             for (int k = 0; k < tamanho; k++)
+    //             {
+    //                 a[]                           
+    //             }
+    //         }
+    //     }
+    // }
 }
